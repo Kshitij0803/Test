020a2f7a7abaf6eb355b5976641e3d75ed59a64f
Kshitij0803 <kshitij0803@gmail.com>
2022-01-24 14:31:42 +0530

    First commit

--- src/test/java/Practice/p2.java
+++ src/test/java/Practice/p2.java
@@ -1,16 +1,18 @@
 package Practice;
 
+import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class p2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, AWTException {
 		System.setProperty("webdriver.chrome.driver", "F:\\Software\\New\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
